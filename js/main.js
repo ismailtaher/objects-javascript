b5d2579e269b793d9a01075ece87b20dd2cf87c8
ismailtaher <ismail.taher@ymail.com>
2023-09-06 19:10:40 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Objects
+
+// Key-value pairs in curly braces
+/* const myObj = { name: "Ismail" };
+
+const anotherObj = {
+  alive: true,
+  answer: 42,
+  hobbies: ["eat", "sleep", "code"],
+  beverage: {
+    morning: "coffee",
+    afternoon: "Iced Tea",
+  },
+  action: function () {
+    return `time for ${this.beverage.morning}`; // keyword this refers to the object so we can access values inside it!
+  },
+};
+console.log(anotherObj.action());
+ */
